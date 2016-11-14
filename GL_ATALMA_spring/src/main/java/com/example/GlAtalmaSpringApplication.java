5aed21e1a2f8901f29592755717db2a7997cb76e
ra1nbowpill <ygor.gallina@gmail.com>
2016-11-14 14:22:44 +0100

    Ajout des prjets Eclipse

---
+++ GL_ATALMA_spring/src/main/java/com/example/GlAtalmaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlAtalmaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlAtalmaSpringApplication.class, args);
+	}
+}
