99f6329bbd3cc30a42a7bff1b77fd08705cd63e1
Baptiste <Baptiste.cv@gmail.com>
2016-12-17 12:32:24 +0100

    Departement : Constructeurs et getter/setter

--- Eclipse/src/core/Departement.java
+++ Eclipse/src/core/Departement.java
@@ -3,6 +3,7 @@
  */
 package core;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,8 +11,8 @@ import java.util.List;
  *
  */
 public class Departement {
-	public List<Enseignant> listeDesEnseignants;
-	public List<Parcours> listeDesParcours;
+	public List<Enseignant> listeDesEnseignants ;
+	public List<Parcours> listeDesParcours ;
 	public String nom;
 	
 	public Departement(List<Enseignant> listeDesEnseignants, List<Parcours> listeDesParcours, String nom) {
@@ -19,6 +20,26 @@ public class Departement {
 		this.listeDesEnseignants = listeDesEnseignants;
 		this.listeDesParcours = listeDesParcours;
 		this.nom = nom;
+	}
+	public Departement(String nom) {
+		super();
+		listeDesEnseignants = new ArrayList<Enseignant>();
+		listeDesParcours = new ArrayList<Parcours>();
+		this.nom = nom;
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public List<Enseignant> getListeDesEnseignants() {
+		return listeDesEnseignants;
+	}
+	public List<Parcours> getListeDesParcours() {
+		return listeDesParcours;
 	}
 	
 	
